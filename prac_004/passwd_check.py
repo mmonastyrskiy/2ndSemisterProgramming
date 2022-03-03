e26c8fd62bf0875767e459c515980f28007369bc
Fallenhacker29 <56514528+Fallenhacker29@users.noreply.github.com>
2022-03-03 18:12:33 +0300

    Pushing examples and my code to the repo

--- prac_004/passwd_check.py
+++ prac_004/passwd_check.py
@@ -13,36 +13,38 @@ name.grid(row=0,column=2,columnspan=3)
 passwd_l = tkinter.Label(text="Пароль ").grid(row=1,column=0)
 passwd = tkinter.Entry(window,show="*")
 passwd.grid(row=1,column=2,columnspan=3)
+class Connection:
+	def __init__(self,name,password):
+		self.name = name
+		self.password = password
+	def Auth(self):
+		login = self.name
+		password = self.password
 
-def Auth():
-    global name
-    global passwd
-    login = name.get()
-    password = passwd.get()
+		import mysql.connector as conn
+		try:
+			mydb=conn.connect(host='127.0.0.1',database='new',user='root',password='')
+			c = mydb.cursor()
+		except Exception as e:
+			messagebox.showerror(message=e)
 
-    import mysql.connector as conn
-    try:
-        mydb=conn.connect(host='127.0.0.1',database='new2',user='root',password='')
-        c = mydb.cursor()
-    except Exception as e:
-        messagebox.showerror(message=e)
+		c.execute(f"SELECT * FROM user WHERE name = '{login}'")
+		user = c.fetchall()
+		print(user)
+		if len(user) == 0:
+			messagebox.showwarning(title="",message="Неверное имя пользователя")
+		else:
+			c.execute(f"SELECT * FROM user WHERE name = '{login}' AND password = '{password}'")
+			record = c.fetchall()
+			if len(record) == 0:
+				messagebox.showerror(message="Неверный пароль")
+			else:
+				callback = tkinter.Tk()
+				callback.resizable(False,False)
+				callback.title("Успех")
+				Ans = tkinter.Label(text = f"Здравствуйте,{login}")
+				callback.mainloop()
+user = Connection(name.get(),passwd.get())
 
-    c.execute(f"SELECT * FROM user WHERE name = '{login}'")
-    user = c.fetchall()
-    print(user)
-    if len(user) == 0:
-        messagebox.showwarning(title="",message="Неверное имя пользователя")
-    else:
-        c.execute(f"SELECT * FROM user WHERE name = '{login}' AND password = '{password}'")
-        record = c.fetchall()
-        if len(record) == 0:
-            messagebox.showerror(message="Неверный пароль")
-        else:
-            callback = tkinter.Tk()
-            callback.resizable(False,False)
-            callback.title("Успех")
-            Ans = tkinter.Label(text = f"Здравствуйте,{login}")
-            callback.mainloop()
-
-B = tkinter.Button(window,text="Войти",command=Auth).grid(row=2,column=1)
+B = tkinter.Button(window,text="Войти",command=user.Auth).grid(row=2,column=1)
 window.mainloop()
