ee169180b7014cf3f30a3295fc64eb911f1d7e17
Fallenhacker29 <56514528+Fallenhacker29@users.noreply.github.com>
2022-03-15 19:27:48 +0300

    Homework 5

--- prac_004/passwd_check.py
+++ prac_004/passwd_check.py
@@ -5,6 +5,7 @@ try:
     import os
     import configparser
     import logging
+    import re
 except ImportError as e:
     os.command(f"pip install {e.split()[-1]}")
 
@@ -13,6 +14,10 @@ CONFIG_FILE = "config.ini"
 
 logging.basicConfig(filename='access.log')
 logging.basicConfig(format='%(asctime)s %(message)s')
+
+class NotSuitadbleData(Exception):
+    def __init__(self,message):
+        self.message = message
 
 try:
     config = configparser.ConfigParser()
@@ -32,7 +37,8 @@ class User:
         self.login = login
         self.password = password
         if self.Auth():
-            customer_data = self.Fetch()
+            self.customer_data = self.Fetch()
+
     def Fetch(self) -> list:
         try:
             mydb = conn.connect(host = host, database=dbname,user=login,password=password)
@@ -51,10 +57,10 @@ class User:
         email = data[0][5]
         phone = data[0][6]
         global users
-        data = [id_,self.login,surname,name,secondname,birth_date,email,phone]
-        logging.warning(f"successfully fetched data:{id_},{self.login},{surname},{name},{secondname},{birth_date},{email},{phone}")
+        data = [id_,self.login,self.password,surname,name,secondname,birth_date,email,phone]
+        logging.warning(f"successfully fetched data:{id_},{self.login},{self.password},{surname},{name},{secondname},{birth_date},{email},{phone}")
         users.append(data)
-        print(users[-1])
+        #print(users[-1])
         return data
 
     def Auth(self) -> bool:
@@ -81,13 +87,180 @@ class User:
                 callback = tkinter.Tk()
                 callback.resizable(False,False)
                 callback.title("Успех")
-                Ans = tkinter.Label(callback,text = f"Здравствуйте,{self.login}")
+                callback.geometry("500x500")
+                Ans = tkinter.Label(callback,text = f"Здравствуйте,{self.login}").grid(row=0,column=0,columnspan=3)
+                ed = tkinter.Button(callback,text = "Edit profile", command = lambda: self.EditGUI()).grid(row=1,column=0)
                 logging.warning(f"User:{self.login} successfully logged in ")
-                Ans.pack()
                 return True
 
                 callback.mainloop()
+
+    def NewPasswd(self, id_, old,new,repeat):
+        try:
+            if (old != new) and (new == repeat) and (len(new) <= 20):
+                mydb=conn.connect(host=host,database=dbname,user=login,password=password)
+                c = mydb.cursor()
+                c.execute(f"UPDATE login SET password = '{new}' WHERE id = {id_}")
+                logging.warning(f"password on {id_} changed from {old} to {new}")
+            else:
+                raise NotSuitadbleData("Passord is not suitable")
+        except Exception as e:
+            messagebox.showerror(message=e)
+
+
+
+    def Checker(self,old,new):
+        try:
+            if (old != new ) and (len(new) < 20):
+                return True
+            else: 
+                raise NotSuitadbleData(f"{new} is not not suitable")
+        except Exception as e:
+            messagebox.showerror(message=e)
+
+
+    def EmailValidator(self,email):
+        try:
+            regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+            if (re.fullmatch(regex,email)) and (len(email) < 30):
+                return True
+            else:
+                raise NotSuitadbleData("Enter valid email")
+        except Exception as e:
+            messagebox.showerror(message=e)
+
+    def PhoneValidator(self,phone):
+        try:
+            regex = r'/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/'
+            if (len(phone) <= 20):
+                return True
+            else:
+                raise NotSuitadbleData("Enter valid phone")
+        except Exception as e:
+            messagebox.showerror(message=e)
+
+    def DateValidator(self,date):
+        try:
+            regex =r'^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$'
+            if (re.fullmatch(regex,date)):
+                return True
+            else:
+                raise NotSuitadbleData("Enter valid date")
+        except Exception as e:
+            messagebox.showerror(message=e)
+
+    def SubmitChanges(self,old,new):                       ## [id_,self.login,self.password,surname,name,secondname,birth_date,email,phone]
+        try:
+            mydb=conn.connect(host=host,database=dbname,user=login,password=password)
+            c = mydb.cursor()
+        except Exception as e:
+            messagebox.showerror(message=e)
+        id_ = old[0]
+        for idx in range(0,len(old)):
+            if str(new[idx]) != str(old[idx]):
+                if idx == 1:
+                    #print(f"1 \t {old[idx]}\t{new[idx]}")
+                    if self.Checker(old[idx],new[idx]):
+                        c.execute(f"UPDATE login SET name = '{str(new[idx])}' WHERE id = {id_}")
+                        mydb.commit()
+                        logging.warning(f"Some data changed on {id_} from {old} to {new}")
+                elif idx == 3:
+                    #print(f"3 \t {old[idx]}\t{new[idx]}")
+                    if self.Checker(old[idx],new[idx]):
+                        c.execute(f"UPDATE user_data set surname = '{str(new[idx])}' WHERE user_id = {id_}")
+                        mydb.commit()
+                        logging.warning(f"Some data changed on {id_} from {old} to {new}")
+                elif idx == 4:
+                    #print(f"4 \t {old[idx]}\t{new[idx]}")
+                    if self.Checker(old[idx],new[idx]):
+                        c.execute(f"UPDATE user_data set name = '{str(new[idx])}' WHERE user_id = {id_}")
+                        mydb.commit()
+                        logging.warning(f"Some data changed on {id_} from {old} to {new}")
+                elif idx ==5:
+                    #print(f"5 \t {old[idx]}\t{new[idx]}")
+                    if self.Checker(old[idx],new[idx]):
+                        c.execute(f"UPDATE user_data set secondname = '{str(new[idx])}' WHERE user_id = {id_}")
+                        mydb.commit()
+                        logging.warning(f"Some data changed on {id_} from {old} to {new}")
+                elif idx ==6:
+                    #print(f"6 \t {old[idx]}\t{new[idx]}")
+                    if self.DateValidator(new[idx]):
+                        c.execute(f"UPDATE user_data set birth_date = '{str(new[idx])}' WHERE user_id = {id_}")
+                        mydb.commit()
+                        logging.warning(f"Some data changed on {id_} from {old} to {new}")
+                elif idx == 7:
+                    #print(f"7 \t {old[idx]}\t{new[idx]}")
+                    if self.EmailValidator(new[idx]):
+                        c.execute(f"UPDATE user_data set email = '{str(new[idx])}' WHERE user_id = {id_}")
+                        mydb.commit()
+                        logging.warning(f"Some data changed on {id_} from {old} to {new}")
+                elif idx == 8:
+                    #print(f"8 \t {old[idx]}\t{new[idx]}")
+                    if self.PhoneValidator(new[idx]):
+                        c.execute(f"UPDATE user_data set telephone = '{str(new[idx])}' WHERE user_id = {id_}")
+                        mydb.commit()
+                        logging.warning(f"Some data changed on {id_} from {old} to {new}")
+            else:
+                idx +=1
         
+    def EditGUI(self):
+        GUI = tkinter.Tk()
+        GUI.resizable(False,False)
+        GUI.title("Изменить настройки профиля")
+        GUI.geometry("500x1000")
+
+        login_l = tkinter.Label(GUI,text="Логин ").grid(row=0,column=0)
+        login = tkinter.Entry(GUI)
+        login.grid(row=0,column=2,columnspan=3)
+
+        passwd_l = tkinter.Label(GUI,text="Пароль текущий:").grid(row=1,column=0)
+        passwd_l_N = tkinter.Label(GUI,text="Пароль Новый:").grid(row=2,column=0)
+        passwd_l_N_R = tkinter.Label(GUI,text="Повторите новый пароль").grid(row=3,column=0)
+
+
+        passwd = tkinter.Entry(GUI,show="*")
+        passwd.grid(row=1,column=2,columnspan=3)
+
+        passwd_N = tkinter.Entry(GUI,show="*")
+        passwd_N.grid(row=2,column=2,columnspan=3)
+
+        passwd_N_R = tkinter.Entry(GUI,show="*")
+        passwd_N_R.grid(row=3,column=2,columnspan=3)
+
+        name_l = tkinter.Label(GUI,text="Имя ").grid(row=4,column=0)
+        name = tkinter.Entry(GUI)
+        name.grid(row=4,column=2,columnspan=3)
+
+        surname_l = tkinter.Label(GUI,text="Фамилия").grid(row=5,column=0)
+        surname = tkinter.Entry(GUI)
+        surname.grid(row=5,column=2,columnspan=3)
+
+        secondname_l = tkinter.Label(GUI,text="Отчество").grid(row=6,column=0)
+        secondname = tkinter.Entry(GUI)
+        secondname.grid(row=6,column=2,columnspan=3)
+
+        date_l = tkinter.Label(GUI,text="Дата рождения").grid(row=7,column=0)
+        date = tkinter.Entry(GUI)
+        date.grid(row=7,column=2,columnspan=3)
+
+        email_l = tkinter.Label(GUI,text="Email ").grid(row=8,column=0)
+        email = tkinter.Entry(GUI)
+        email.grid(row=8,column=2,columnspan=3)
+
+        phone_l = tkinter.Label(GUI,text="Телефон ").grid(row=9,column=0)
+        phone = tkinter.Entry(GUI)
+        phone.grid(row=9,column=2,columnspan=3)
+        B = tkinter.Button(GUI,text="Apply",command = lambda: self.SubmitChanges(data,[data[0]] + [x.get() for x in form]) if (len(passwd_N.get()) == 0) else (self.NewPasswd(data[0],passwd.get(),passwd_N.get(),passwd_N_R.get()))).grid(row=10,column=1)
+        data = self.Fetch()
+        form = [login,passwd,surname,name,secondname,date,email,phone]
+        for i in range(0,len(data)):
+            #print(i)
+            if i == 0:
+                continue
+            else:
+                form[i-1].insert(0,str(data[i]))
+        GUI.mainloop()
+
 
 
 def main():
