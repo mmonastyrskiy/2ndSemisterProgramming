be133fe3a5ce46daf32b62c2b46b5cd8a0f2f872
Fallenhacker29 <56514528+Fallenhacker29@users.noreply.github.com>
2022-04-23 22:28:30 +0300

    Parcer almost fixed

--- prac_004/parce.py
+++ prac_004/parce.py
@@ -8,10 +8,20 @@ import re
 from tqdm import tqdm
 import sys
 import pandas as pd
+import urllib3
+import os.path as path
 from copy import copy
+import os
+URL = "https://store77.net/"
+
+obj_pointer = 0
+
+urllib3.disable_warnings()
 #import colorama as color
 blacklisted_url = ["https://store77.net/chasy_apple_watch_nike_se"]
 goods = []
+links = []
+
 sys.setrecursionlimit(100)
 class NotOKResponseCode(Exception):
     def __init__(self,message):
@@ -23,6 +33,8 @@ logging.basicConfig(filename='access.log')
 logging.basicConfig(format='%(asctime)s %(message)s')
 
 #color.init(autoreset=True)
+
+
 
 try:
     config = configparser.ConfigParser()
@@ -39,16 +51,12 @@ except Exception:
 
 def GetDataFromURL(url: str) -> str:
     try:
-        print("Touch")
         response = r.get(url,verify=False)
-        #print(response.status_code)
     except Exception:
-        #print(response.ststus_code)
         raise NotOKResponseCode("Connection died")
     if response.status_code == 200:
         return response.text
     else:
-        print(response.status_code)
         raise NotOKResponseCode(f"Something is wrong with the url:{url}, page returned code: {response.status_code}")
 
 def ParceStore77Net_Sections(url):
@@ -56,7 +64,6 @@ def ParceStore77Net_Sections(url):
         resp = GetDataFromURL(url)
     except NotOKResponseCode:
         print("Seems like host is down, check manually")
-        exit(1)
     soup = bs(resp,"lxml")
     sections = soup.find_all("ul",class_ = "catalog_menu_sub_third")
     #print(sections)
@@ -96,8 +103,50 @@ def ParceStore77Net_Sections(url):
 
 
 
+
+def ParceStore77Deps(file):
+    chars = []
+    chars_dict = {}
+    try:
+        df = pd.read_excel(file)
+    except Exception as e:
+        print(e)
+    df = df.reset_index()
+    for idx, row in tqdm(df.iterrows()):
+        try:
+            data = GetDataFromURL(row["link"].replace("//","/"))
+            #print(row["link"])
+            soup = bs(data,"lxml")
+            images = soup.find_all("img",class_="")
+        except Exception as e:
+            print("Image load fail")
+            print(e)
+        links=[]
+        global obj_pointer
+        try:
+            for img in images:
+                print(img["src"])
+                img_data = GetDataFromURL(img["src"]).text
+                with open("." + os.sep + "img" + os.sep + str(obj_pointer) + ".jpg","wb") as f:
+                    f.write(bytes(img_data))
+        except Exception:
+            pass
+        try:
+            data = GetDataFromURL(row["link"])
+            print(row["link"])
+            soup = bs(data,"lxml")
+            Description = soup.find("div",class_ = "col-sm-6 wrap_descr_b").string
+        except Exception as e:
+            print("Description loading Error")
+            print(e)
+    obj_pointer +=1
+
+
+
+
+
 def Insert_to_database(file):
-    df.read_excel(file)
+    df = pd.read_excel(file)
     try:
         mydb=conn.connect(host=host,database=dbname,user=login,password=password)
         c = mydb.cursor()
@@ -105,7 +154,7 @@ def Insert_to_database(file):
         print(e)
     df = df.reset_index()
     for index, row in df.iterrows():
-        c.execute(f"INSERT INTO catalog (art,name,price,brand,category) VALUES ('{row['arts'][1:-2]}','{row['name'][1:-2]}','{str(row['row'][0:-1])}',{row['brand'][1:-2]},{row['cat'][1:-2]})")
+        #c.execute(f"INSERT INTO catalog (art,name,price,brand,category,links) VALUES ('{row['arts'][1:-2]}','{row['name'][1:-2]}','{str(row['price'][0:-1])}',{row['brand'][1:-2]},{row['cat'][1:-2]},{row['link'][1:-2]})")
         mydb.commit()
 
 
@@ -117,9 +166,10 @@ def SaveData():
     price = [elem[2] for elem in goods]
     brand = [elem[3] for elem in goods]
     cat = [elem[4] for elem in goods]
+    dlink = [elem[-1] for elem in goods]
     if not path.exists("res.xlsx"):
         for i in tqdm(range(0,len(arts))):
-            df = pd.DataFrame({"arts":arts,"name":name,"price":price,"brand":brand,"cat":cat})
+            df = pd.DataFrame({"arts":arts,"name":name,"price":price,"brand":brand,"cat":cat,"link":dlink})
             df.to_excel("res.xlsx")
             Insert_to_database("res.xlsx")
     else:
@@ -128,9 +178,11 @@ def SaveData():
 
 
 def ParceStore77Net_EachSection(link):
-    print("New line")
+    #print("New line")
+    parced_links = []
+    l = copy(link)
     try:
-        response = GetDataFromURL(link)
+        response = GetDataFromURL(link+"/")
     except RecursionError:
         print("Website died")
         return
@@ -139,9 +191,18 @@ def ParceStore77Net_EachSection(link):
         ParceStore77Net_EachSection(link)
 
     soup = bs(response,"lxml")
-    data = soup.find_all("a",class_="bp_hover_text_but_cart",href=True)
+    data = soup.find_all("div",class_="blocks_product_fix_w")
+    for div in data:
+        link = div.find("a",href=True)
+        href = link["href"]
+        parced_links.append(href)
+    idx = 0
+    data = soup.find_all("a",class_="bp_hover_text_but_cart")
     #print(data)
+    #print(link)
+    global links
     data = str(data).split(' onclick="dataLayer.push({')
+    link = data[0]
     #print(data[3])
     parced_data = []
     for elem in data:
@@ -149,13 +210,9 @@ def ParceStore77Net_EachSection(link):
         elems = elem.split("\n")
         property_ = []
         for line in elems:
-            #print(line)
             if ":" in line:
                 property_.append(tuple([re.sub(r"\s+"," ",line.split(":")[0]),re.sub(r"\s+"," ",line.split(":")[1]).split("//")[0]]))
-                print(property_)
-            else:
-                #print(line)
-                pass
+                #print(property_)
         property_ = property_[9:-1]
         try:
             art = property_[2][1]
@@ -163,7 +220,11 @@ def ParceStore77Net_EachSection(link):
             price = property_[3][1]
             brand = property_[4][1]
             cat = property_[5][1]
-            parced_data.append([art,name,price,brand,cat])
+            dlink = parced_links[idx]
+            idx += 1
+            print(dlink)
+            global URL
+            parced_data.append([art,name,price,brand,cat,URL[:-1] + dlink])
         except Exception:
             pass
     global goods
@@ -172,7 +233,7 @@ def ParceStore77Net_EachSection(link):
 
 
 def ParceStore77Net():
-    URL = "https://store77.net/"
+    global URL
     sections = ParceStore77Net_Sections(URL)
     for page in tqdm(sections):
         print(page)
@@ -183,6 +244,8 @@ def ParceStore77Net():
     global goods
     print(len(goods))
     SaveData()
+    ParceStore77Deps("res.xlsx")
+
 
 
 
